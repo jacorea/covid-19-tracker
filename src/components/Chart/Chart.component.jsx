13c0926fb807357220222ff8d890ad1af2d8b875
jcorea <jacorea410@gmail.com>
2020-04-24 14:14:31 -0700

    add api call to fetch daily covid19 data in our api directory. Add rechart.js to use Line chart. Updated Chart component to display daily covid19 data in terms of deaths and infected.

--- src/components/Chart/Chart.component.jsx
+++ src/components/Chart/Chart.component.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { fetchedDailyData } from '../../api';
+
+//packages
+import { Line, Bar } from 'react-chartjs-2';
+
 import classes from './Chart.module.css';
 
+
 const Chart = () => {
+  //USED TO SET STATE
+  const [dailyData, setDailyData] = useState([]);
+
+  useEffect(()=> {
+    const fetchedAPI = async() => {
+      // const dailyData = await fetchedDailyData();
+      //used to set the state sort of like this.setState({})
+      setDailyData(await fetchedDailyData());
+    }
+    fetchedAPI(); //calling our function populates our dailyData
+  }, [])
+
+  const lineChart = (
+    dailyData.length ? (<Line 
+      data={{
+        labels: dailyData.map(({ date }) => date ),
+        datasets: [{
+          data: dailyData.map(({ confirmed}) => confirmed),
+          label: 'Infected',
+          borderColor: '#3333ff',
+          fill: true,
+        },
+        {
+          data: dailyData.map(({ deaths }) => deaths ),
+          label: 'Deaths',
+          borderColor: 'rgba(255,0,0,1.0)',
+          backgroundColor: 'rgba(255,0,0,0.50)',
+          fill: true,
+        }]
+      }}
+    />) : null
+  )
+
   return (
-    <div>
-      <h1>Chart</h1>
+    <div className={classes.container}>
+      {lineChart}
     </div>
   )
 }
